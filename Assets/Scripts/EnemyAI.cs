3d971c41daa4f7cae4afa1e5ea514ccc9a300fde
mrtclgst <mrtclgst@gmail.com>
2023-04-03 07:55:07 +0300

    .

--- Assets/Scripts/EnemyAI.cs
+++ Assets/Scripts/EnemyAI.cs
@@ -3,7 +3,19 @@ using UnityEngine;
 
 public class EnemyAI : MonoBehaviour
 {
+    private enum State
+    {
+        WaitingForEnemyTurn,
+        TakingTurn,
+        Busy,
+    }
+
+    private State _state;
     private float _timer = 0;
+    private void Awake()
+    {
+        _state = State.WaitingForEnemyTurn;
+    }
 
     private void Start()
     {
@@ -17,15 +29,50 @@ public class EnemyAI : MonoBehaviour
             return;
         }
 
-        _timer -= Time.deltaTime;
-        if (_timer >= 0f)
+        switch (_state)
         {
-            TurnSystem.Instance.NextTurn();
+            case State.WaitingForEnemyTurn:
+
+                break;
+            case State.TakingTurn:
+
+                _state = State.Busy;
+                _timer -= Time.deltaTime;
+                if (_timer <= 0f)
+                {
+                    _state = State.Busy;
+                    TakeEnemyAIAction(SetStateTakingTurn);
+                    TurnSystem.Instance.NextTurn();
+                }
+
+                break;
+            case State.Busy:
+
+                break;
+            default:
+                break;
         }
+
+
+    }
+
+    private void SetStateTakingTurn()
+    {
+        _timer = .5f;
+        _state = State.TakingTurn;
+    }
+
+    private void TakeEnemyAIAction(Action OnEnemyAIActionComplete)
+    {
+
     }
 
     private void OnEventTurnChanged(object sender, EventArgs e)
     {
-        _timer = 2f;
+        if (!TurnSystem.Instance.IsPlayerTurn())
+        {
+            _state = State.TakingTurn;
+            _timer = 2f;
+        }
     }
 }
---
+++ Assets/Scripts/UnitManager.cs
@@ -0,0 +1,39 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+///INFO
+///->Usage of UnitManager script: 
+///ENDINFO
+
+public class UnitManager : MonoBehaviour
+{
+    #region Public Variables
+
+    #endregion
+
+    #region Private Variables
+
+    private List<Unit> _unitList;
+    private List<Unit> _friendlyUnitList;
+    private List<Unit> _enemyUnitList;
+
+    #endregion
+
+    #region Cached
+
+    #endregion
+
+    #region Unity Methods
+
+    #endregion
+
+    #region Events
+
+    #endregion
+
+    #region Functions
+
+    #endregion
+}
